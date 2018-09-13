852d8424ff05972ef05a48186628a627a19d091d
Shiraj Gill <shirajgill@cmail.carleton.ca>
2018-09-12 22:07:31 -0400

    Removed Ranks enum test since no trivial enum and added Rank enums

---
+++ src/main/java/blackjack/Ranks.java
@@ -0,0 +1,17 @@
+package blackjack;
+
+public enum Ranks {
+	 ACE,
+   TWO,
+   THREE,
+   FOUR,
+   FIVE,
+   SIX,
+   SEVEN,
+   EIGHT,
+   NINE,
+   TEN,
+   JACK,
+   QUEEN,
+   KING
+}
--- src/test/java/core/CardTest.java
+++ src/test/java/core/CardTest.java
@@ -4,22 +4,30 @@ import junit.framework.*;
 
 public class CardTest extends TestCase {
 
-	public void testEqual() {
+	public void testNotEqual() {
 		//Card A is a King of Hearts 
-		Card cardA = new Card("KH", 10);
+		Card cardA = new Card(Suits.HEART, Ranks.KING);
 		//Card B is a King of Spades
-		Card cardB = new Card("KS", 10);
+		Card cardB = new Card(Suits.SPADES, Ranks.KING);
 		//These two cards aren't not the same
 		assertEquals(false, cardA.equals(cardB));
 	}
 	
-	public void testValue() {
-		//Card A is a King of Hearts 
-		Card cardA = new Card("KH", 10);
-		//A king should equate to 10
-		assertEquals(10, cardA.value());
+	public void testEqual() {
+		//Card A is a 5 of Hearts 
+		Card cardA = new Card(Suits.HEARTS, Ranks.FIVE);
+		//Card B is a 5 of Hearts
+		Card cardB = new Card(Suits.HEARTS, Ranks.FIVE);
+		//These two cards are the same
+		assertEquals(true, cardA.equals(cardB));
 	}
 	
 	
+	public void testValue() {
+		//Card A is a King of Hearts 
+		Card cardA = new Card(Suits.HEART, Ranks.KING);
+		//A king should equate to 10
+		assertEquals(10, cardA.value());
+	}
 
 }
